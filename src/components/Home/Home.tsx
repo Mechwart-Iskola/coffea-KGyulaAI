450d1080d5c33a4860f1d671553d554c44237d32
Kovács Gyula <kovacs.gyula@burg.lo.hu>
2025-04-02 09:13:02 +0200

    új

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import { homeImages } from "../../data/data"
+import "./home.css"
+import HomeStickerImg from "../../../public/img/home-sticker.svg"
 
 const Home = () => {
-    {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/}
-    
-  {/*A kép több másik képből tevődik össze ezeket egy tömbben találod a data mappában.*/}
+  {/*A komponens megírásához szükséges css osztályokat a home.css-ben találod.*/ }
 
-  {/*A kép hátterében egy alakzat van.*/}
+  {/*A kép több másik képből tevődik össze ezeket egy tömbben találod a data mappában.*/ }
 
-  {/*A gomb mutasson az about részre */}
+  {/*A kép hátterében egy alakzat van.*/ }
+
+  {/*A gomb mutasson az about részre */ }
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1 className="home__title">COLD COFFEE</h1>
+      <div className="home__images">
+        <div className="home__shape"></div>
+        {homeImages.map(image => (
+          <img src={image.src} alt={image.alt} className={image.className} />
+        ))}
+      </div>
+      <img src={HomeStickerImg} alt="" className="home__Sticker" />
+      <div className="home__data">
+        <p className="home__description">
+          Find delicious hot and cold coffees with the
+          best varieties, calm the pleasure and enjoy
+          a good coffee, order now.
+        </p>
+        <a href="#about" className="button">Learn More</a>
+      </div>
+    </div>
   )
 }
 
